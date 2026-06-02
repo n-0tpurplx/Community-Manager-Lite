3463248a26de07783f1158674c4fa08186ce8815
n0tpurplx_ <silvertandluccc@gmail.com>
2026-06-02 19:51:50 +0200

    Refactor login function and handle Discord authentication

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function fakeLogin() {
-  const status = document.getElementById("authStatus");
+function loginWithDiscord() {
+  window.location.href =
+    "https://clm-backend-mi99.onrender.com/auth/discord";
+}
 
-  status.innerText = "Status: Logged in (fake user)";
+// read backend redirect result
+const params = new URLSearchParams(window.location.search);
+
+const status = document.getElementById("authStatus");
+
+if (params.get("auth") === "success") {
+  const user = params.get("user");
+
+  status.innerText = "Status: Logged in as " + user;
   status.style.color = "lime";
+}
+
+if (params.get("auth") === "failed") {
+  status.innerText = "Status: Login failed";
+  status.style.color = "red";
 }
